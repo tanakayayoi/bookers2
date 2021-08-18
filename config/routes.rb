8ae41e63f80ce5cab7bd3b044697c62ef538079b
tanakayayoi <march.yayoi31@gmail.com>
2021-08-18 11:22:40 +0000

    [Update]viewファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :books, only: [:new, :create, :index, :show]
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

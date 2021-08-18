80aba98a397c4c82c374126f65d0ad5d1385bca0
tanakayayoi <march.yayoi31@gmail.com>
2021-08-18 09:22:45 +0000

    [Update]deviseを使用するmodelの作成

--- db/migrate/20210818090503_devise_create_users.rb
+++ db/migrate/20210818090503_devise_create_users.rb
@@ -32,6 +32,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :name
+      t.text :introduction
+      t.integer :profile_image_id
+
 
       t.timestamps null: false
     end

5074ac82e298bb0ed4257eaf9fa99c2a0ddf1d15
tanakayayoi <march.yayoi31@gmail.com>
2021-08-19 13:27:23 +0000

    [Update]User info途中まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,24 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path(params[:id])
   end
 
   def index
+    @book = Book.new
   end
 
   def show
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end

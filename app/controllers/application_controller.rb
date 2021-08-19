89b00537cbbdb01ef4bd3f23335a8a5cdc45808a
tanakayayoi <march.yayoi31@gmail.com>
2021-08-19 09:43:25 +0000

    [Update]loginページの編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    if current_user
-      user_path(@user)
-    else
-      user_registration_path
-    end
+    user_path(resource)
   end
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :introduction, :profile_image_id])
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
   end
 
 end

decaf6765616a65ce80a630ccb17aab87fa3551a
tanakayayoi <march.yayoi31@gmail.com>
2021-09-01 08:33:27 +0000

    [Update]いいね機能のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:index, :show, :create, :update, :edit, :destroy]
+  resources :books, only: [:index, :show, :create, :update, :edit, :destroy] do
+    resources :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
   get "home/about" => "homes#about" ,as: "about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

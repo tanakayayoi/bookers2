1b622dad0e0b8c39889854cca573d3d6563b04bf
tanakayayoi <march.yayoi31@gmail.com>
2021-09-02 08:43:10 +0000

    [Update]ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
+   resource :relationships, only: [:create, :destroy]
+   get 'followings' => 'relationships#followings', as: 'followings'
+   get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   get "home/about" => "homes#about" ,as: "about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

ae60c465c8c9171071960071688e08a2dd5dffc4
tanakayayoi <march.yayoi31@gmail.com>
2021-09-02 12:12:31 +0000

    [update]フォローボタン、フォロワー数の表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  has_many :relationships, class_name: "Relationships", foreign_key: "follower_id", dependent: :destroy
-  has_many :following,through: :relationships, source: :followed
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
 
   attachment :profile_image
 
@@ -27,7 +27,7 @@ class User < ApplicationRecord
   end
 
   def following?(user)
-    following.include?(user)
+    followings.include?(user)
   end
 
   validates :name, {uniqueness: true, length: {in: 2..20}}
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,11 @@ Rails.application.routes.draw do
     resources :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
    resource :relationships, only: [:create, :destroy]
    get 'followings' => 'relationships#followings', as: 'followings'
    get 'followers' => 'relationships#followers', as: 'followers'
   end
-
   get "home/about" => "homes#about" ,as: "about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

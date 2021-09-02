bb106c9dc9e876e88f3f85ebf2657f3c1ed71dfc
tanakayayoi <march.yayoi31@gmail.com>
2021-09-02 08:29:18 +0000

    [Update]アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :relationships, class_name: "Relationships", foreign_key: "follower_id", dependent: :destroy
+  has_many :following,through: :relationships, source: :followed
 
   attachment :profile_image
 

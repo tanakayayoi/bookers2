b8d3420acb8631bc990ff3e7133742c24b06a9be
tanakayayoi <march.yayoi31@gmail.com>
2021-09-02 05:17:35 +0000

    [Update]コメント削除機能の実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,28 @@
 class BookCommentsController < ApplicationController
 
   def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_to request.referrer
   end
 
   def destroy
+    @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    
+    if @book_comment.user == current_user
+      @book_comment.destroy
+      redirect_to request.referrer
+    else
+      redirect_to books_path
+    end
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
   end
 
 end

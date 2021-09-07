58e65ec91afbd5481c1ad23420f42851338b8c80
tanakayayoi <march.yayoi31@gmail.com>
2021-09-07 14:13:23 +0000

    [Fix]いいね機能の非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,12 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_to request.referrer
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referrer
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :books, only: [:index, :show, :create, :update, :edit, :destroy] do
-    resources :favorites, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update] do
